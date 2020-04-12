b8d8ea72d46302feaac3489cb2d82a07a225a5b2
sachin singh <sachin.singh@antino.io>
2020-04-13 00:01:08 +0530

    Adding login and user registration in backend

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,63 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+
+//models
+const User = require("../models/User");
 
 //@route GET api/auth
 //@desc Get loggged in user
 //@access Private
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
-});
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Please include a valid password").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(500).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ ok: false, msg: "invalid credentials" });
+      }
+      //compare password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(404).json({ ok: false, msg: "invalid credentials" });
+      }
+
+      // require jwt token
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("SecretKey"),
+        {
+          expiresIn: 3600000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ msg: "Server error" });
+    }
+  }
+);
 
 //@route POST api/auth
 //@desc Auth user & get token
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const connectDB = require("./config/db");
 
 const app = express();
 
+app.use(express.json());
+
 //Connect Database
 
 connectDB();
